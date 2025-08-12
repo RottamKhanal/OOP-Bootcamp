11f06ff69965a91c6303949f16c1f9e14cba740c
Rottam Khanal <rottamkhanal1@gmail.com>
2025-08-12 10:17:01 +0545

    ..

---
+++ SecondSem/1st.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Enter three different numbers: ";
+    cin >> a >> b >> c;
+
+    int sum = a + b + c;
+    cout << "The sum is: " << sum << endl;
+
+    return 0;
+}
